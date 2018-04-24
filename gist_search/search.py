fb6410b08b02b26af6369c9a9f526beceaa7e329
jasonmeeks <jasonmeeks42@gmail.com>
2018-04-23 21:56:02 -0500

    Update search.py

--- gist_search/search.py
+++ gist_search/search.py
@@ -3,5 +3,5 @@ from gist_search.utils import get_gists
 
 def search_gists(username, description=None, file_name=None):
     if not description and not file_name:
-        print("At least one serach parameter must be specified")
+        print("At least one search parameter must be specified")
         return
